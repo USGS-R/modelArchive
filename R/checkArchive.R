cdbcf264926ccca6342b850311a51c93821c35ff
T. Joe Mills <tmills@usgs.gov>
2016-08-10 10:29:00 -0600

    removed annoyign print statements

--- R/checkArchive.R
+++ R/checkArchive.R
@@ -9,74 +9,72 @@
 #'@export
 #'
 checkArchive <- function(filePath = "modelArchive") {
-  wd <- getwd()
   
-  setwd(paste0(getwd(),"/",filePath))
   errors <- character()
   
-  print("Checking input directory")
+  #print("Checking input directory")
   
-  if(dir.exists("input"))
+  if(dir.exists(paste0(getwd(),"/",filePath,"/input")))
   {
     if(length(list.files("input")) == 0)
     {
-      errors <- c(errors,"Input directory contains no input files")
+      errors <- c(errors," Input directory contains no input files")
     } else {"OK"}
     
   } else{errors <- c(errors,"No input directory found")}
   
-  print("Checking output directory")
+  #print("Checking output directory")
   
-  if(dir.exists("output"))
+  if(dir.exists(paste0(getwd(),"/",filePath,"/output")))
   {
     if(length(list.files("output")) == 0)
     {
-      errors <- c(errors,"output directory contains no output files")
+      errors <- c(errors," output directory contains no output files")
     } else {"OK"}
     
   } else{errors <- c(errors,"No output directory found")}
   
-  print("Checking R_files directory")
+  #print("Checking R_files directory")
   
-  if(dir.exists("R_files"))
+  if(dir.exists(paste0(getwd(),"/",filePath,"/R_files")))
   {
     if(length(list.files("R_files")) == 0)
     {
       errors <- c(errors,"R_files directory contains no files")
     } else {"OK"}
     
-  } else{errors <- c(errors,"No R_files directory found, run getR()")}
+  } else{errors <- c(errors," No R_files directory found, run getR()")}
   
-  print("Checking scripts directory")
+  #print("Checking scripts directory")
   
-  if(dir.exists("scripts"))
+  if(dir.exists(paste0(getwd(),"/",filePath,"/scripts")))
   {
     if(length(list.files("scripts")) == 0)
     {
-      errors <- c(errors,"scripts directory contains no files")
+      errors <- c(errors," scripts directory contains no files")
     } else {"OK"}
     
-  } else{errors <- c(errors,"No scripts directory found, run copyScripts()")}
+  } else{errors <- c(errors," No scripts directory found, run copyScripts()")}
   
-  print("Checking scripts directory")
+  #print("Checking scripts directory")
   
-  if(dir.exists("packrat"))
+  if(dir.exists(paste0(getwd(),"/",filePath,"/packrat")))
   {
     if(length(list.files("packrat")) == 0)
     {
-      errors <- c(errors,"packrat directory contains no files")
+      errors <- c(errors," packrat directory contains no files")
     } else {"OK"}
     
-  } else{errors <- c(errors,"No packrat directory found, run bundlePackages()")}     
+  } else{errors <- c(errors," No packrat directory found, run bundlePackages()")}     
   
-  if(file.exists("README.txt"))
+  if(file.exists(paste0(getwd(),"/",filePath,"/README.txt")))
   {
-    readme <- scan("README.txt",what="character",sep="\n")
+    readme <- scan(paste0(getwd(),"/",filePath,"/README.txt"),what="character",sep="\n")
     if(length(grep("ADD DESCRIPTION",readme)))
     {
-      c(errors,"Some files do not have descriptions in README.txt")
+      c(errors," Some files do not have descriptions in README.txt")
     }
-  } else{errors <- c(errors,"No README.txt found, run makeReadMe()")}  
+  } else{errors <- c(errors," No README.txt found, run makeReadMe()")}  
   
   if(length(errors) == 0) {
     print("Model archive complete")
